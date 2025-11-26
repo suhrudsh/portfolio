feb1bb341f597063f49a725898d1b2047c351b08
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-11-26 11:05:27 +0530

    fix: Adjust layout classes in FeaturedProject component for better responsiveness

--- src/components/home/FeaturedProject.jsx
+++ src/components/home/FeaturedProject.jsx
@@ -12,12 +12,12 @@ export function FeaturedProject({ link, imgSrc, imgAlt, title, children }) {
         alt={imgAlt}
         width={1920}
         height={1080}
-        className="rounded-2xl object-cover transition-transform duration-300 ease-in-out group-hover:scale-105 lg:col-span-7 lg:col-start-4 xl:col-span-6 xl:col-start-5 xl:row-span-2"
+        className="xl:row-span- rounded-2xl object-cover transition-transform duration-300 ease-in-out group-hover:scale-105 lg:col-span-7 lg:col-start-4 2xl:col-start-5"
       />
       <h3 className="group-hover:text-accent mt-6 text-3xl font-black transition-colors duration-300 ease-in-out lg:col-span-4 lg:col-start-2 lg:row-start-2 lg:mt-0 lg:text-5xl xl:col-span-5 xl:col-start-3 xl:row-start-3 xl:text-7xl">
         {title}
       </h3>
-      <p className="mt-2 flex flex-col gap-2 text-pretty lg:col-span-5 lg:col-start-7 lg:row-start-2 lg:mt-0 xl:col-span-4 xl:col-start-9 xl:row-start-3">
+      <p className="mt-2 flex flex-col gap-2 text-pretty lg:col-span-5 lg:col-start-8 lg:row-start-2 lg:mt-0 xl:col-span-4 xl:col-start-9 xl:row-start-3 xl:text-xl">
         {children}
         {children && (
           <>
